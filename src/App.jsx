6ee7b60166b962f4e5e3ccdf38212fee6c959149
omer-kuluc <omerkuluc96@gmail.com>
2026-03-09 13:57:14 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="container">
+        sasa
+      </div>
+    </>
+  )
+}
+
+export default App
